049b85ef1a8573ca483b7f240d80073af053b002
Mobashir Farhan <farhanmobashir007@gmail.com>
2022-01-29 13:26:03 +0530

    adds basic folder structure

--- main.js
+++ main.js
@@ -4,5 +4,3 @@ document.querySelector("#app").innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
-
-console.log("Hello");
