40afec1b82ff703eba6041be364b08f2d746a616
Mobashir Farhan <farhanmobashir007@gmail.com>
2022-02-13 14:19:47 +0530

    feat:adds dismiss toast code to docs
    -adds the tost dismiss logic to the docs site
    -make some minor tweak in the css of the toast button

--- src/js/ampereui.js
+++ src/js/ampereui.js
@@ -7,3 +7,11 @@ alertDismiss.forEach((item) => {
     e.target.parentNode.style.display = "none";
   });
 });
+
+// * -----   js for toast ----- //
+const toastCloseButton = document.querySelectorAll(".close-btn");
+toastCloseButton.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.target.parentNode.parentNode.style.display = "none";
+  });
+});
